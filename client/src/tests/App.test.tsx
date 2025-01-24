24f65873fba698f96aeae453729bc8cceceb4605
bill haigh <williamhaigh297@gmail.com>
2025-01-24 16:06:10 +0000

    TEST/CHORE: initial config and setup for front end tests && simple render test for APP component

---
+++ client/src/tests/App.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import App from "./../App"
+
+
+describe("App render test", () => {
+  test("Should render the app component", () => {
+    render(
+      <App />
+    );
+    expect(screen.getByText("Vite + React")).toBeInTheDocument();
+  })
+
+})
