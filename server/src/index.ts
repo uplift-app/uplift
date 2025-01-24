0ff14eba6d970651880b1a12396960343f93e3a1
Burak Tas <b.tas93@googlemail.com>
2025-01-24 17:32:50 +0100

    add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import setRouting from "./router";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.SERVER_PORT;
+const CLIENT_PORT = process.env.CLIENT_PORT;
 
+app.use(cors({ origin: `http://localhost:${CLIENT_PORT}` }));
 app.use(express.json());
 setRouting(app);
 
