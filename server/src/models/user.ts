8867c0d224b1f9eb7428cda6efef63f4436018b3
Burak Tas <b.tas93@googlemail.com>
2025-01-27 15:43:50 +0100

    db models: add timestamps

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,11 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 import { IUser } from "../interfaces";
 
-const UserSchema: Schema = new Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const UserSchema: Schema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model<IUser>("User", UserSchema);
 export default User;
