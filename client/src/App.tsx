9a1702ef5b86b38bdbf13921fb35269a62c283e3
Burak Tas <b.tas93@googlemail.com>
2025-01-29 12:26:02 +0100

    add analysis api call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import ActivityInput from "./components/ActivityInput";
 import MoodInput from "./components/MoodInput";
+import { getAnalysis } from "./lib/ApiService";
+import { useAnalysisDataContext } from "./contexts/AnalysisDataContext";
 
 function App() {
+  const { setAnalysisData } = useAnalysisDataContext();
+  useEffect(() => {
+    getAnalysis().then((data) => setAnalysisData(data));
+  }, []);
+
   return (
-    <div className="  flex items-center justify-center">
+    <div className="flex items-center justify-center">
       <ActivityInput />
       <MoodInput />
     </div>
@@ -11,4 +19,3 @@ function App() {
 }
 
 export default App;
-
