5ac52386044b1a9fa6d985746e5111c38285b900
Tommy White <twhite306@gmail.com>
2025-01-30 13:46:01 +0000

    FIX: Typing change in interfaces

--- client/src/components/PositiveEffects.tsx
+++ client/src/components/PositiveEffects.tsx
@@ -1,16 +1,25 @@
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "./ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./ui/card";
 import { useAnalysisDataContext } from "@/contexts/AnalysisDataContext";
-import { Select,  SelectContent,
+import {
+  Select,
+  SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-  SelectGroup, } from "./ui/select";
+  SelectGroup,
+} from "./ui/select";
 import { useEffect, useState } from "react";
 
 interface Ieffect {
   moodType: string;
   moodTime: string;
-  avgIntensity: number;
+  avg_intensity: number;
   activities: string[];
 }
 
@@ -18,7 +27,7 @@ const PositiveEffects = () => {
   const [mood, setMood] = useState<string>("show all");
   const [time, setTime] = useState<string>("show all");
   const [filteredEffects, setFilteredEffects] = useState<Ieffect[] | []>([]);
-  const {analysisData} = useAnalysisDataContext();
+  const { analysisData } = useAnalysisDataContext();
 
   useEffect(() => {
     if (!analysisData || Object.keys(analysisData).length === 0) {
@@ -26,19 +35,21 @@ const PositiveEffects = () => {
     }
     setFilteredEffects(filterPositiveEffects(mood, time));
   }, [mood, time, analysisData]);
+  console.log(analysisData.positiveEffects);
 
-  const {positiveEffects} = analysisData;
-  console.log(positiveEffects)
+  const { positiveEffects } = analysisData;
+  console.log(positiveEffects);
 
   const filterPositiveEffects = (mood?: string, time?: string): Ieffect[] => {
     if (mood === "show all" && time === "show all") {
       return positiveEffects;
     }
-    return positiveEffects.filter(effect =>
-      (mood === "show all" || effect.moodType === mood) &&
-      (time === "show all" || effect.moodTime === time)
+    return positiveEffects.filter(
+      (effect) =>
+        (mood === "show all" || effect.moodType === mood) &&
+        (time === "show all" || effect.moodTime === time)
     );
-  }
+  };
 
   const formatFilteredEffects = (filteredEffects: Ieffect[]) => {
     const moodMapping: { [key: string]: string } = {
@@ -50,75 +61,90 @@ const PositiveEffects = () => {
     return filteredEffects.map((effect) => {
       const formattedMood = moodMapping[effect.moodType];
       return (
-        <Card className="w-[200px]">
-        <CardHeader>
-          <CardTitle>{formattedMood} {effect.moodTime === "all day" ? effect.moodTime : 'in the '+ effect.moodTime}.</CardTitle>
-          <CardDescription>
-            The average intensity of your mood during this time is {effect.avg_intensity.toFixed(1)}.
+        <Card className='w-[200px]'>
+          <CardHeader>
+            <CardTitle>
+              {formattedMood}{" "}
+              {effect.moodTime === "all day"
+                ? effect.moodTime
+                : "in the " + effect.moodTime}
+              .
+            </CardTitle>
+            <CardDescription>
+              The average intensity of your mood during this time is{" "}
+              {effect.avg_intensity.toFixed(1)}.
             </CardDescription>
-        </CardHeader>
-        <CardContent>
-        {effect.activities.length > 1 ? (
-        <>
-        <p>The activites that positively affected this are:</p>
-        <ul>
-        {effect.activities.map((activity) => {
-          return (
-            <li>{activity[0].toUpperCase() + activity.slice(1)}.</li>
-          )
-        })}
-        </ul>
-        </>
-      ):(<p>The activity that positively affected this is: {effect.activities[0]}</p>)}
-        </CardContent>
+          </CardHeader>
+          <CardContent>
+            {effect.activities.length > 1 ? (
+              <>
+                <p>The activites that positively affected this are:</p>
+                <ul>
+                  {effect.activities.map((activity) => {
+                    return (
+                      <li>{activity[0].toUpperCase() + activity.slice(1)}.</li>
+                    );
+                  })}
+                </ul>
+              </>
+            ) : (
+              <p>
+                The activity that positively affected this is:{" "}
+                {effect.activities[0]}
+              </p>
+            )}
+          </CardContent>
         </Card>
-      
-    );
-    })
-  }
+      );
+    });
+  };
 
   return (
-    <Card className="w-[500px]">
+    <Card className='w-[500px]'>
       <CardHeader>
         <CardTitle>Positive Effects</CardTitle>
         <CardDescription>
           Understand the positive effects of your activities and moods.
         </CardDescription>
       </CardHeader>
-      <CardContent className="space-y-4">
+      <CardContent className='space-y-4'>
         <Select onValueChange={(value) => setMood(value)}>
           <SelectTrigger>
-            <SelectValue placeholder="Select a mood" />
+            <SelectValue placeholder='Select a mood' />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
-              <SelectItem value="energetic">Energy</SelectItem>
-              <SelectItem value="happy">Happiness</SelectItem>
-              <SelectItem value="relaxed">Relaxation</SelectItem>
-              <SelectItem value="show all">Show All</SelectItem>
+              <SelectItem value='energetic'>Energy</SelectItem>
+              <SelectItem value='happy'>Happiness</SelectItem>
+              <SelectItem value='relaxed'>Relaxation</SelectItem>
+              <SelectItem value='show all'>Show All</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
         <Select onValueChange={(value) => setTime(value)}>
           <SelectTrigger>
-            <SelectValue placeholder="Select a time" />
+            <SelectValue placeholder='Select a time' />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
-              <SelectItem value="morning">Morning</SelectItem>
-              <SelectItem value="afternoon">Afternoon</SelectItem>
-              <SelectItem value="evening">Evening</SelectItem>
-              <SelectItem value="all day">All Day</SelectItem>
-              <SelectItem value="show all">Show All</SelectItem>
+              <SelectItem value='morning'>Morning</SelectItem>
+              <SelectItem value='afternoon'>Afternoon</SelectItem>
+              <SelectItem value='evening'>Evening</SelectItem>
+              <SelectItem value='all day'>All Day</SelectItem>
+              <SelectItem value='show all'>Show All</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
-        <div className="flex flex-wrap ">
-        {filteredEffects.length > 0 ? formatFilteredEffects(filteredEffects) : <p>No positive effects found for the selected mood and time.</p>}
+        <div className='flex flex-wrap '>
+          {filteredEffects.length > 0 ? (
+            formatFilteredEffects(filteredEffects)
+          ) : (
+            <p>No positive effects found for the selected mood and time.</p>
+          )}
         </div>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
 export default PositiveEffects;
