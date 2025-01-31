1aaf806b465cbf922a1a077a2e4b77987a208403
Burak Tas <b.tas93@googlemail.com>
2025-01-31 12:02:39 +0100

    update mood and activity models

--- server/scripts/mood.py
+++ server/scripts/mood.py
@@ -13,8 +13,8 @@ def generate_dates(start_date, end_date):
         current_date += timedelta(days=1)
 
 # Mood data settings
-mood_types = ["happy", "relaxed", "energetic"]
-mood_times = ["morning", "afternoon", "evening", "all day"]
+mood_types = ["happiness", "stress", "energy"]
+mood_times = ["morning", "afternoon", "evening", "night", "all day"]
 
 # Generate data for User 1 (last three months)
 end_date_user1 = datetime.today()
