ff573b2876a4e9a1b1900a6e95e54e4c71300b12
Burak Tas <b.tas93@googlemail.com>
2025-01-31 12:13:04 +0100

    update navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,12 +4,27 @@ import { Link } from "react-router-dom";
 function Navbar() {
   const { isSignedIn } = useUser();
   return (
-    <div className="border-b-4 border-yellow-500">
-      <div className="flex justify-between p-4">
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/entries">Entries</Link>
-        <Link to="/">Home</Link>
-        {isSignedIn ? <UserButton showName /> : <Link to="/login">Login</Link>}
+    <div className="border-b-[3px] border-yellow-500">
+      <div className="flex p-4 items-center">
+        <div className="flex gap-8 font-semibold w-[16rem]">
+          <Link to="/dashboard" className="hover:underline">
+            Dashboard
+          </Link>
+          <Link to="/entries" className="hover:underline">
+            Entries
+          </Link>
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+        </div>
+        <div className="font-bold text-xl uppercase mx-auto">Uplift</div>
+        <div className="w-[16rem] flex justify-end">
+          {isSignedIn ? (
+            <UserButton showName />
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </div>
       </div>
     </div>
   );
