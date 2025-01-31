569d2da66849d184b4ef9ece9cf08add98cb9f40
Tommy White <twhite306@gmail.com>
2025-01-31 10:12:32 +0000

    FEAT: format function for usernames implemented

--- client/src/components/pages/DashBoard.tsx
+++ client/src/components/pages/DashBoard.tsx
@@ -2,16 +2,13 @@ import { RedirectToSignIn, useUser } from "@clerk/clerk-react";
 import ActivityInput from "../cards/ActivityInput";
 import MoodInput from "../cards/MoodInput";
 import ChartViewer from "../cards/ChartViewer";
+import { formatName } from "@/lib/utils";
 
 function DashBoard() {
   const { user, isSignedIn } = useUser();
   if (user && isSignedIn) {
-    const username = user.username
-      ? user.username
-          .split("_")
-          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-          .join(" ")
-      : "User";
+    let username = user.username ? user.username : "User";
+    username = formatName(username);
     return (
       <>
         <p className='justify-self-center text-4xl'>Welcome, {username}!</p>
--- client/src/components/pages/EntriesPage.tsx
+++ client/src/components/pages/EntriesPage.tsx
@@ -2,6 +2,7 @@ import { RedirectToSignIn, useUser } from "@clerk/clerk-react";
 import ActivityInput from "../cards/ActivityInput";
 import MoodInput from "../cards/MoodInput";
 import { RecentEntries } from "../RecentEntries";
+import { formatName } from "@/lib/utils";
 
 function EntriesPage() {
   /**
@@ -21,23 +22,25 @@ function EntriesPage() {
    */
   const { user, isSignedIn } = useUser();
   if (user && isSignedIn) {
+    let username = user.username ? user.username : "User";
+    username = formatName(username);
     return (
       <>
-        <p className="text-center text-4xl py-4">Welcome, {user.username}!</p>
+        <p className='text-center text-4xl py-4'>Welcome, {username}!</p>
 
-        <div className="flex gap-4 px-[10rem]">
-          <div className="w-fit flex flex-col items-center gap-4">
-            <div className="flex flex-col items-center w-full bg-[#e0f2fe] text-black rounded-lg p-8 gap-2 shadow-md">
-              <p className="text-3xl">
+        <div className='flex gap-4 px-[10rem]'>
+          <div className='w-fit flex flex-col items-center gap-4'>
+            <div className='flex flex-col items-center w-full bg-[#e0f2fe] text-black rounded-lg p-8 gap-2 shadow-md'>
+              <p className='text-3xl'>
                 “One of the keys to happiness is a bad memory.”
               </p>
-              <p className="italic text-lg">Rita Mae Brown</p>
+              <p className='italic text-lg'>Rita Mae Brown</p>
             </div>
-            <div className="w-full bg-[#e0f2fe] rounded-lg p-4 shadow-md">
-              <h2 className="text-black font-semibold text-lg">
+            <div className='w-full bg-[#e0f2fe] rounded-lg p-4 shadow-md'>
+              <h2 className='text-black font-semibold text-lg'>
                 Track your activity and mood
               </h2>
-              <div className="flex justify-between">
+              <div className='flex justify-between'>
                 <ActivityInput />
                 <MoodInput />
               </div>
